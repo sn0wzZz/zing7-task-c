ed42ce49c382ebdaee0f7e1e36580b3afdcbccfe
sn0wzZz <m.stanchev1999@abv.bg>
2024-04-18 15:51:12 +0300

    Minor services fix

--- src/app/others/Others.tsx
+++ src/app/others/Others.tsx
@@ -4,7 +4,7 @@ import Service from './Service'
 
 export default function Others() {
   return (
-    <div className='w-full my-10 xl:my-20 xl:mx-32 mx-2'>
+    <div className=' my-10 xl:my-20 xl:mx-32 mx-2'>
       <h2>Други услуги</h2>
       <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10  justify-items-center'>
         {services.map((img,i)=> <Service key={i} img={img}/>)}
