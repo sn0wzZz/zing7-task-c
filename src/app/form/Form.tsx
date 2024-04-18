f981b9b9c91373ffd557b86979f86c8c0a0d5aec
sn0wzZz <m.stanchev1999@abv.bg>
2024-04-18 15:13:09 +0300

    Initial commit

--- src/app/form/Form.tsx
+++ src/app/form/Form.tsx
@@ -3,7 +3,7 @@ import { FiMail } from 'react-icons/fi'
 
 export default function Form() {
   return (
-    <div className='h-auto w-[80%] md:w-[70%] lg:w-[50%] xl:w-[60%] 2xl:w-[60%] flex  flex-col justify-center text-center py-10 xl:py-24'>
+    <div className='h-auto w-[80%] md:w-[70%] lg:w-[50%] xl:w-[60%] 2xl:w-[45%] flex  flex-col justify-center text-center py-10 xl:py-24'>
       <h1 className='text-white my-10'>
         Готиви ли сте да работим заедно?
       </h1>
@@ -13,14 +13,14 @@ export default function Form() {
       </p>
       <div className=' my-16 flex flex-nowrap justify-center'>
         <div className='flex items-center bg-white rounded-l-xl pl-3 xl:pl-5'>
-          <FiMail color='grey' size={30} />
+          <FiMail color='grey' size={20} />
           <input
             className=' p-3 w-[10rem] xl:w-auto xl:p-5 xl:text-xl outline-none  border-none bg bg-transparent'
             type='text'
             placeholder='Мобилен телефон'
           />
         </div>
-        <button className='p-3 xl:p-5 xl:px-10 bg-lime-200 xl:text-xl rounded-r-xl border-0'>
+        <button className='p-3 xl:p-5 xl:px-10 bg-lime-200 xl:text-xl rounded-r-xl border-0 font-bold'>
           Изпрати
         </button>
       </div>
